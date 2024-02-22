6d97c354b62ace00a120c3300f22d4feec25203b
moolraaj <raaj73906@gmail.com>
2024-02-22 18:28:30 +0530

    done

--- src/components/home/SearchBox.tsx
+++ src/components/home/SearchBox.tsx
@@ -8,6 +8,7 @@ export default function SearchBox() {
                         <div className="search-heading">
                             <h1 className='top-title'>May i take your order</h1>
                             <p>this is a creator</p>
+                            <p>this is a new creator</p>
                         </div>
                         <div className="search-box">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
