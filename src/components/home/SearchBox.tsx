c0c6434489753b2c5d31473b490f2afecbf24ea3
moolraaj <raaj73906@gmail.com>
2024-02-22 19:00:38 +0530

    done

--- src/components/home/SearchBox.tsx
+++ src/components/home/SearchBox.tsx
@@ -8,6 +8,7 @@ export default function SearchBox() {
                         <div className="search-heading">
                             <h1 className='top-title'>May i take your order</h1>
                             <p>Hello coders</p>
+                            <h1>this is raaj file ti check the code</h1>
                         </div>
                         <div className="search-box">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
